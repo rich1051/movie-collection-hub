238200bcb149cb3a7339c0be6f24c7705d060014
Will Richardson <rich1051@umn.edu>
2023-06-16 10:55:53 -0500

    Formatting cleanup

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -1,25 +1,24 @@
 import { useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 
-function MovieItem ({movie}) {
-    const dispatch = useDispatch();
-    const history = useHistory();
+function MovieItem({ movie }) {
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const handleClick = () => {
-        dispatch({
-            type: 'FETCH_DETAILS',
-            payload: movie.id
-        });
-        history.push('/details')
-    }
+  const handleClick = () => {
+    dispatch({
+      type: "FETCH_DETAILS",
+      payload: movie.id,
+    });
+    history.push("/details");
+  };
 
-
-    return(
-        <div>
-            <h3>{movie.title}</h3>
-            <img onClick={handleClick} src={movie.poster} alt={movie.title}/>
-        </div>
-    )
+  return (
+    <div>
+      <h3>{movie.title}</h3>
+      <img onClick={handleClick} src={movie.poster} alt={movie.title} />
+    </div>
+  );
 }
 
 export default MovieItem;
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,35 +1,30 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import './MovieList.css'
-import MovieItem from '../MovieItem/MovieItem';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import "./MovieList.css";
+import MovieItem from "../MovieItem/MovieItem";
 
 function MovieList() {
+  const dispatch = useDispatch();
+  const moviesReducer = useSelector((store) => store.moviesReducer);
 
-    const dispatch = useDispatch();
-    const moviesReducer = useSelector(store => store.moviesReducer);
+  useEffect(() => {
+    dispatch({ type: "FETCH_MOVIES" });
+  }, []);
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-
-    return (
-        <main>
-            <h1>MovieList</h1>
-            <section className="movies">
-                {moviesReducer.map(movie => {
-                    return (
-                        <div key={movie.id} >
-{/* MovieItem here? Get rid of h3 and img below: */}
-                            {/* <h3>{movie.title}</h3>
-                            <img src={movie.poster} alt={movie.title}/> */}
-                            <MovieItem movie={movie}/>
-                        </div>
-                    );
-                })}
-            </section>
-        </main>
-
-    );
+  return (
+    <main>
+      <h1>MovieList</h1>
+      <section className="movies">
+        {moviesReducer.map((movie) => {
+          return (
+            <div key={movie.id}>
+              <MovieItem movie={movie} />
+            </div>
+          );
+        })}
+      </section>
+    </main>
+  );
 }
 
 export default MovieList;
